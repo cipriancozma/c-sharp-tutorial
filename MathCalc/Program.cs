d2480f88e06904edb74a9663d1e993015c781354
CCozma <ciprian.cozma@cognyte.com>
2024-07-19 14:35:45 +0300

    added reverse name and math calc prj

---
+++ MathCalc/Program.cs
@@ -0,0 +1,56 @@
+﻿using static System.Console;
+
+WriteLine("Welcome to Console Math Calc!");
+var isFirstRun = true;
+double firstNumber = 0;
+
+var operators = new[] { "+", "-", "/", "%", "*", "=" };
+
+while (true)
+{
+    if(isFirstRun)
+    {
+        WriteLine("Enter number: ");
+        var firstInput = ReadLine();
+
+        if (!double.TryParse(firstInput, out firstNumber))
+        {
+            WriteLine("Invalid number!");
+            continue;   
+        }
+    }
+
+    WriteLine($"The first number is: {firstNumber}");
+
+    WriteLine("Enter second number: ");
+    var secondInput = ReadLine();
+
+    if(!double.TryParse(secondInput, out double secondNumber))
+    {
+        WriteLine("Invalid number!");
+        continue;
+    }
+
+    WriteLine($"Second number is: {secondNumber}");
+
+    WriteLine($"Enter operator: ({string.Join(",", operators)})");
+    var mathOperator = ReadLine();
+
+    if(!isOperatorValid(mathOperator))
+    {
+        WriteLine("Invalid operator!");
+        continue;
+    }
+}
+
+bool isOperatorValid(string mathOperator)
+{
+    foreach (var op in operators)
+    {   
+        if (op.Equals(mathOperator))
+        {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ ReverseName/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Text;
+
+
+string name;
+
+do
+{
+    Console.WriteLine("Write the name you want reversed: ");
+    name = Console.ReadLine();
+
+} while (name == "");
+
+string reversedName = ReverseName(name);
+PrintReverseName(reversedName);
+
+string ReverseName(string name)
+{
+    var stringBuilder = new StringBuilder();
+
+    for(int i = name.Length - 1; i >= 0; i--)
+    {
+        stringBuilder.Append(name[i]);
+    }
+
+    return stringBuilder.ToString();
+}
+
+void PrintReverseName(string name)
+{
+    for (int i = 0; i < name.Length; i++)
+    {
+        Console.WriteLine(name[i]);
+    }
+}
